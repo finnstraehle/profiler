2f8ef4206d01b34e08885e1ea59d2bc8495eb36a
Finn Straehle <finn.straehle1@gmx.ch>
2023-12-10 19:16:37 +0100

    added same header styling to all pages and created route for resumes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :resumes, only: %i[index]
   resources :cover_letters, only: %i[index]
   resources :applications, only: %i[index]
 end
