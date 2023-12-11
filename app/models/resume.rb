0771c3f2685ca463a1955e7fa443f730eb08add2
Finn Straehle <finn.straehle1@gmx.ch>
2023-12-11 16:50:32 +0100

    added sections to resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,3 +1,4 @@
 class Resume < ApplicationRecord
   belongs_to :user
+  has_many :resume_entries, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ puts "Creating users..."
     user: user # assigning the user to the cover letter
   )
   cover_letter.save! # saving the cover letter
-  rand(4..6).times do # creating 1 to 3 cover letter entries for each cover letter
+  rand(4..6).times do # creating 4 to 6 cover letter entries for each cover letter
     cover_letter_entry = CoverLetterEntry.new( # creating a cover letter entry
       text: Faker::Quote.matz + " " + Faker::Quote.matz, # generating a random text
       cover_letter: cover_letter # assigning the cover letter to the cover letter entry
@@ -49,6 +49,18 @@ puts "Creating users..."
     user: user # assigning the user to the resume
   )
   resume.save! # saving the resume
+  rand(1..3).times do # creating 1 to 3 resume entries for each resume
+    resume_entry = ResumeEntry.new( # creating a resume entry
+      title: Faker::Company.name, # generating a random title
+      subtitle: Faker::Job.title, # generating a random subtitle
+      location: Faker::Address.city, # generating a random location
+      start: Faker::Date.between(from: 20.days.ago, to: Date.today), # generating a random start date
+      end: Faker::Date.between(from: 20.days.ago, to: Date.today), # generating a random end date
+      is_active: true, # setting is_active to true
+      resume: resume # assigning the resume to the resume entry
+    )
+    resume_entry.save! # saving the resume entry
+  end
 end
 
 puts 'done!' # printing done when finished, to know when the seed is succesfully executed
